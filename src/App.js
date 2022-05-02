0ffe7f3aebcfb17c08893d20dae17ff76e32930b
santraaj <santra.jantunen@gmail.com>
2022-05-03 02:09:54 +0300

    Delete lisätty

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import Carlist from './components/Carlist';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 
 function App() {
   return (
     <div className="App">
-   <Carlist />
+       <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            CarShop
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Carlist />
     </div>
   );
 }
--- src/components/Carlist.js
+++ src/components/Carlist.js
@@ -1,10 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { AgGridReact } from 'ag-grid-react'
 import'ag-grid-community/dist/styles/ag-grid.css'
 import'ag-grid-community/dist/styles/ag-theme-material.css';
+import Button from '@mui/material/Button';
 
 export default function Carlist() {
     const [cars, setCars] = useState([]);
+    const gridRef = useRef();
 
     useEffect(() => fetchData(), []);
 
@@ -14,27 +16,39 @@ export default function Carlist() {
         .then(data => setCars(data._embedded.cars))
     }
 
+    const deleteCar = (link) => {
+      if (window.confirm('Are you sure?')) {
+        fetch(link, {method: 'DELETE' })
+        .then(res => fetchData())
+        .catch(err => console.error(err))
+      }
+    }
     //mandatory params: columns and data?
 
     const columns = [
-      { headerName: 'Brand', field: 'brand', sortable: true, filter: true},
-      { headerName: 'Model', field: 'model', sortable: true, filter: true},
-      { headerName: 'Color', field: 'color', sortable: true, filter: true},
-      { headerName: 'Fuel', field: 'fuel', sortable: true, filter: true},
-      { headerName: 'Year', field: 'year', sortable: true, filter: true},
-      { headerName: 'Price', field: 'price', sortable: true, filter: true}
+      { headerName: 'Brand', field: 'brand', sortable: true, filter: true },
+      { headerName: 'Model', field: 'model', sortable: true, filter: true },
+      { headerName: 'Color', field: 'color', sortable: true, filter: true },
+      { headerName: 'Fuel', field: 'fuel', sortable: true, filter: true },
+      { headerName: 'Year', field: 'year', sortable: true, filter: true },
+      { headerName: 'Price', field: 'price', sortable: true, filter: true },
+      { headerName: '', maxWidth: 100, field: '_links.self.href', 
+      cellRenderer: (params) => <Button color="error" size="small" onClick={() => deleteCar(params.value)}>Delete</Button> }
     ]
 
     return (
       <div className='ag-theme-material'
       style={{
         height: '700px',
-        width: '80%',
+        width: '100%',
         margin: 'auto'}}
       >
         <AgGridReact
           columnDefs={columns}
           rowData={cars}
+          rowSelection='single'
+          ref={gridRef}
+          onGridReady={ params => gridRef.current = params.api }
         >
         </AgGridReact>
       </div>  
